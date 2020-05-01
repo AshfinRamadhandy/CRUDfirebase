4c47233af16566043a4c4ce74befb43b44744890
AshfinRamadhandy <ashfinramadhandy10@gmail.com>
2020-05-01 14:02:31 +0700

    edit config firebase

---
+++ src/firebase/config.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyDtoslqkHxB9aD4ElsstBdx4KLWSlYv-RI",
+  authDomain: "crudfirebasepbf.firebaseapp.com",
+  databaseURL: "https://crudfirebasepbf.firebaseio.com",
+  projectId: "crudfirebasepbf",
+  storageBucket: "crudfirebasepbf.appspot.com",
+  messagingSenderId: "343563375155",
+  appId: "1:343563375155:web:fcfe29b43f23336a9b8946"
+};
+
+export default firebaseConfig;  
